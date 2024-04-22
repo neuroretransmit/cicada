c3a4f9885fa2dbdc852ef815a6f99a864221d70f
null <null@MacBook-Pro.local>
2024-04-21 20:50:12 -0700

    [2014] fix decode.sh/page numbers/gematriaprimus.py and update README.md

--- 2014/005-third-onion/decode.sh
+++ 2014/005-third-onion/decode.sh
@@ -13,14 +13,20 @@ outguess -r 03.jpg 03.jpg.asc
 # decrypt message, remove not from Cicada for hex, and convert to binary for hint image
 gpg -d 03.jpg.asc | tail -n +6 | xxd -r -p > 03.jpg.asc.jpg
 
+# use infotomb server status to extract next two pages
+xxd -r -p infotomb-server-status.hex > infotomb-server-status.bin
+# reverse
+./reversebytes.py infotomb-server-status.bin infotomb-server-status-reversed.bin
+# compare reversed/forward
+cmp -l infotomb-server-status.bin infotomb-server-status-reversed.bin
+# extract page 4
+dd if=infotomb-server-status-reversed.bin bs=1 count=$(./findendofjpg.py infotomb-server-status-reversed.bin) status=noxfer > 04.jpg
+outguess -r 04.jpg 04.jpg.bin
+
 # convert server status hex to data
 xxd -r -p server-status.hex > server-status.bin
-# reverse byte for reversed/appended image
-./reversebytes.py server-status.bin server-status-reversed.bin
-# compare files byte-by-byte
-cmp -l server-status.bin server-status-reversed.bin > server-status-cmp.txt
-# extract the magic square
+# Extract ONLY the first image - page 5 - no extra garbage
+dd if=server-status.bin bs=1 count=$(./findendofjpg.py server-status.bin) status=noxfer > 05.jpg
+# extract magic square text file
 dd if=server-status.bin bs=1 skip=$((0x00521e4)) count=357 status=noxfer | rev | xxd -p -r > square.txt
-# Extract ONLY the first image - no extra garbage
-dd if=server-status.bin bs=1 count=$(./findendofjpg.py server-status.bin) status=noxfer > 04.jpg
 ./gematriaprimus.py
